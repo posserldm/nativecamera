c8bee4c08c7711771a5d1de55af7f0969f67677c
posser <1782342391@qq.com>
2023-05-28 13:00:07 +0800

    添加相册缩略图加载功能

--- app/src/main/java/posser/nativecamera/view/ThumbnailActivity.kt
+++ app/src/main/java/posser/nativecamera/view/ThumbnailActivity.kt
@@ -3,6 +3,7 @@ package posser.nativecamera.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import android.widget.GridLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import posser.nativecamera.R
@@ -17,27 +18,55 @@ class ThumbnailActivity : AppCompatActivity() {
     private var mediaInfoList: List<MediaInfo>? = null
     private val showList = mutableListOf<MediaInfo>()
     private var recAdapter: ThumbnailAdapter? = null
+    private val layoutManager: GridLayoutManager by lazy { GridLayoutManager(this@ThumbnailActivity, 4) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityThumbnailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initRecycleView()
+        binding.tnPhotoRec.setOnTouchListener { _, event ->
+            onTouchEvent(event)
+            false
+        }
     }
 
     private fun initRecycleView() {
         mediaInfoList = intent.getParcelableArrayListExtra("mediaInfoList")
         Log.i("posserTest", "ThumbnailActivity mediaList = $mediaInfoList")
         mediaInfoList?.let {
-            if (it.size >= 50) {
-                showList.addAll(it.subList(0, 50))
+            if (it.size >= 30) {
+                showList.addAll(it.subList(0, 30))
             } else {
                 showList.addAll(it)
             }
             recAdapter = ThumbnailAdapter(showList, this)
             binding.tnPhotoRec.let { rec ->
                 rec.adapter = recAdapter
-                rec.layoutManager = GridLayoutManager(this@ThumbnailActivity, 4)
+                rec.layoutManager = layoutManager
             }
         }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        Log.i("posserTest", "onTouchEvent on called!")
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {}
+            MotionEvent.ACTION_MOVE -> {
+                Log.i("posserTest", "相册缩略图在滑动")
+                val vItem = layoutManager.findLastVisibleItemPosition()
+                if (vItem >= (recAdapter!!.itemCount - 15)) {
+                    mediaInfoList?.let {
+                        val l = if (recAdapter!!.itemCount + 15 >= it.size) {
+                            it.subList(recAdapter!!.itemCount, it.size)
+                        } else {
+                            it.subList(recAdapter!!.itemCount, recAdapter!!.itemCount + 15)
+                        }
+                        recAdapter?.appendMediaList(l)
+                    }
+                }
+            }
+            MotionEvent.ACTION_UP -> {}
+        }
+        return super.onTouchEvent(event)
     }
 }
